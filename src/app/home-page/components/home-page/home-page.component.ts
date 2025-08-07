4612e522b466525ddacea1274e9a4f3d9cabc2a0
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-08-07 16:25:35 -0600

    Se sigue creando el header y los botones que llevan a las paginas donde se mostraran los datos

--- src/app/home-page/components/home-page/home-page.component.ts
+++ src/app/home-page/components/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from "../../../header/components/header/header.component";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'home-page',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterLink, RouterLinkActive],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
