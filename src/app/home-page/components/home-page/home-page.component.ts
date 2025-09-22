c7c25fd05d1bb1a8f12a0fbb2da7d0da66373f10
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-09-22 17:57:42 -0600

    FIX & STYLE: Elimine lineas que no se necesitaba y corri la app porque dejo de funcionar, actualize angular a todas sus versiones iguales '20.3.1'

--- src/app/home-page/components/home-page/home-page.component.ts
+++ src/app/home-page/components/home-page/home-page.component.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { HeaderComponent } from "../../../header/header/header.component";
-import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
-import { CardComponent } from "../../../cards/card/card.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'home-page',
-  imports: [HeaderComponent, RouterLink, RouterLinkActive, RouterModule, CardComponent],
+  imports: [RouterModule],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
